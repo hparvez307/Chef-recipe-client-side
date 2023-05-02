16eda15fe70ee6f20538356a7e0e285597e7707a
hparvez307 <hparvez307@gmail.com>
2023-05-02 22:32:21 +0600

    login page

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className='bg-white text-black w-6/12 mx-auto rounded-lg h-[400px]'>
+            <h1 className='text-4xl font-bold pb-28 pt-10'>Login</h1>
+            <form  >
+               
+                <label className='text-2xl mr-3 font-bold'>Email:</label>
+               <input className='border-2  p-3 rounded border-zinc-400 w-[400px] h-10' type="email" name="email" id="email" placeholder='please enter your email' required />
+             
+                <br />
+                <br />
+                <label className='text-2xl mr-3  font-bold '>Password:</label>
+                <input className='border-2  p-3 rounded border-zinc-400 w-[400px] mr-12 h-10' type="password" name="password" id="password" placeholder='please enter password' required />
+                <br />
+                <input type="submit" value="submit" />
+            </form>
         </div>
     );
 };
---
+++ src/component/register/Register.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <form onSubmit={handleOnSubmit}>
+                <input type="email" name="email" id="email" placeholder='please enter email' required />
+                <br />
+                <input type="password" name="password" id="password" placeholder='please enter password' required />
+                <br />
+                <input type="submit" value="submit" />
+            </form>
+        </div>
+    );
+};
+
+export default Register;
